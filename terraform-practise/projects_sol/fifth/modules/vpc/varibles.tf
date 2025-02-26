29bd31457226cfeeaaf6449294187149e11748b8
16monav <naveenthapliyal1989@gmail.com>
2025-02-26 18:10:58 +0100

    all push

--- terraform-practise/projects_sol/fifth/modules/vpc/varibles.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-    type = string  
-}
-
-variable "ami_id" {
-    default = "ami-04b4f1a9cf54c11d0"
-    description = "default ami for ubunutu"
-  
-}
--- terraform-practise/projects_sol/fifth/real_project/main.tf
+++
@@ -1,4 +0,0 @@
-module "vpc" {
-    source = "../modules/vpc"
-    vpc_cidr = "10.0.0.0/16"
-}
